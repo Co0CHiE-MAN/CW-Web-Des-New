a2f5541964ba213dd5ad888159996a541bce76d2
Co0CHiE-MAN <kalindugunathilake123@gmail.com>
2023-02-25 23:05:18 +0530

    first commit

---
+++ Components/index.js
@@ -0,0 +1,9 @@
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(()=> {
+        splash.classList.add('display-none')
+    }, 4000);
+} )
+
+
