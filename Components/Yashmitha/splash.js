8e4a00c19cc224dc68ea29d8a0950ceab3664b4b
𝓝𝓮𝓷𝓾𝔁 𝓑𝓻𝓸 <cncsarduino@gmail.com>
2023-04-02 02:31:04 +1000

    upddated links

--- Components/Yashmitha/splash.js
+++ Components/Yashmitha/splash.js
@@ -3,7 +3,7 @@ const splash = document.querySelector('.splash');
 document.addEventListener('DOMContentLoaded', function() {
     setTimeout(()=> {
         splash.classList.add('display-none')
-    }, 000);
+    }, 4000);
 } )
 
 
