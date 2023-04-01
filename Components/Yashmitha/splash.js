3224edbd48b90bc2567074926d6cdbe25e4c8358
Co0CHiE-MAN <kalindugunathilake123@gmail.com>
2023-04-01 15:44:48 +0530

    initial commit

---
+++ Components/Yashmitha/splash.js
@@ -0,0 +1,11 @@
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(()=> {
+        splash.classList.add('display-none')
+    }, 000);
+} )
+
+
+
+
--- Components/index.js
+++
@@ -1,9 +0,0 @@
-const splash = document.querySelector('.splash');
-
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(()=> {
-        splash.classList.add('display-none')
-    }, 4000);
-} )
-
-
